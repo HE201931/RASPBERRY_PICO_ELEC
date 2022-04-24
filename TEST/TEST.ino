15148a4266427629b9621d7e9f3679a9ec1e889b
NtFallAsleep <71380301+HE201931@users.noreply.github.com>
2022-04-24 12:04:22 +0200

    Add files via upload

--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -8,6 +8,7 @@
 //https://www.arduino.cc/en/serial/write
 //TODO IF TIME : sending size before sending payload
 byte data[4096];
+
 enum PAYLOAD_HELPER : byte
 {
   GET_DISTANCE_CAPTED =   0,
@@ -25,9 +26,40 @@ void loop()
 {
   if(Serial.available() > 0)
   {
-    int rlen = Serial.readBytes(data, 4096);
-    Serial.write(data, 4096);
-    Serial.flush();
+    int rlen = Serial.readBytes(data, 4096);    
+
+    PAYLOAD_HELPER payload_type = (PAYLOAD_HELPER)data[0];
+
+    switch(payload_type)
+    {
+      case SEND_TEXT:
+        send_text_serial_port("Arduino");      
+        break;
+    }
+    //Serial.write(data, 4096);
+    //Serial.flush();
   }
   delay(10);
+}
+
+int send_text_serial_port(String text)
+{  
+    int buffer_return_size = 2 + text.length();
+
+    int buffer_string_size = text.length() + 1;
+    
+    byte buffer_return[buffer_return_size];
+
+    byte buffer_string[buffer_string_size];
+    
+    text.getBytes(buffer_string, buffer_string_size);
+    
+    buffer_return[0] = SEND_TEXT;
+
+    for(int i = 0; i < buffer_string_size ; i++)
+       buffer_return[i + 1] = buffer_string[i];
+
+    Serial.write(buffer_return, buffer_return_size);
+    Serial.flush();  
+    return buffer_return_size;
 }
